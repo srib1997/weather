aa3f97d79619c6128defd7f5689ac862f8ba5ef0
srib1997 <peterf1a08@yahoo.com.hk>
2018-10-03 16:23:48 +0800

    fetch weather website ,  xml change to object

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const parseString = require('xml2js').parseString
 const express = require('express')
+const fetch = require('node-fetch')
 const app = express()
 const port = 3000
+let data = {}
 
-const data = {
-  a: 1,
-  b: 2
-}
+fetch('http://rss.smg.gov.mo/c_ActualWeather_rss.xml')
+  .then(function (res) {
+    return res.text()
+  })
+  .then(function (body) {
+    parseString(body, (err, result) => {
+      data = result
+    })
+  })
 
 app.get('/', (req, res) => res.json(data))
 
