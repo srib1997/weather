6c816b782a25a6c706f2608057fd3970d5eb7b7f
srib1997 <peterf1a08@yahoo.com.hk>
2018-10-04 14:10:31 +0800

    add the weather forcast

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cron = require('node-cron')
 const app = express()
 const port = 3000
 let data = {}
+let data2 = {}
 
 const fetchWeather = function () {
   console.log('fetching...' + new Date())
@@ -18,6 +19,21 @@ const fetchWeather = function () {
       })
     })
 }
+
+const fetchForcast = function () { 
+  fetch('http://rss.smg.gov.mo/c_WForecast_rss.xml')
+    .then(function (res) {
+      return res.text()
+    })
+    .then(function (body) {
+      parseString(body, (err, result) => {
+        data2 = result
+      })
+    })
+}
+
+fetchForcast()
+cron.schedule('0 * * * * *', fetchForcast)
 
 fetchWeather()
 cron.schedule('0 * * * * *', fetchWeather)
@@ -36,5 +52,19 @@ app.get('/', (req, res) => {
   }
   res.json(newResult)
 })
+
+app.get('/forcast', (req, res) => {
+  const description2 = data2.rss.channel[0].item[0].description[0]
+
+   const newResult2 = {
+     pubDate: data2.rss.channel[0].pubDate[0],
+     title: data2.rss.channel[0].item[0].title[0],
+     WeatherDate: description2.match(/預測於 (.*)/)[1],
+     WeatherForecast: description2.match(/天氣乾燥。(.*)/)[0],
+     WeatherDateTomorrow: description2.match(/預測於 (.*)/)[2]
+     }
+  res.json(newResult2)
+}
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
